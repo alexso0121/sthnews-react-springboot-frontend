6ddf83dd6681ed1e7de0e335a8c6e38d7ba59447
alexso0121 <114412149+alexso0121@users.noreply.github.com>
2022-11-14 18:46:18 +0800

    image

--- src/Weathering/weatheritem.jsx
+++ src/Weathering/weatheritem.jsx
@@ -1,6 +1,14 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 function Weatheritem ({week,forecastWeather,forecastMaxtemp,forecastMintemp,forecastWind}) {
-    return ( <><ListGroup.Item><div><span style={{"marginLeft":"100%"}}>{week}</span><span style={{"marginRight":"100%"}}>element</span></div><div>{forecastWeather}</div><div></div>{/* {forecastWeather}{forecastMaxtemp} */}
+    return ( <><ListGroup.Item><Container><Row>
+        <Col /* style={{"marginLeft":"100%"}} */>{week}</Col>
+        <Col /* style={{"marginRight":"100%"}} */>element</Col>
+        </Row>
+        </Container>
+        <Row><Col style={{"marginLeft":"12px"}}>  {forecastWeather}</Col></Row>
+        <Row><Col style={{"marginLeft":"12px"}}>
+              Estimated temperature: {forecastMintemp.value}{forecastMintemp.unit} - {forecastMaxtemp.value}{forecastMaxtemp.unit}</Col></Row>
     </ListGroup.Item></> );
 }
 
