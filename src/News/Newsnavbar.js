d7ebe5ac186dde88e44dad09d8791885fe0df276
alexso0121 <114412149+alexso0121@users.noreply.github.com>
2022-11-14 18:17:01 +0800

    df

--- src/News/Newsnavbar.js
+++ src/News/Newsnavbar.js
@@ -4,20 +4,20 @@ function Newsnavbar() {
   return (
     <Nav justify variant="tabs"  defaultActiveKey="/home">
       <Nav.Item>
-        <Nav.Link style={{'color':'black'}} href="/0"><nobr><strong>Top news</strong></nobr></Nav.Link>
+        <Nav.Link style={{'color':'black', "fontFamily": "sans-serif"}} href="/0"><nobr><strong>Top news</strong></nobr></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link style={{'color':'black'}} href="/1"><strong>World</strong></Nav.Link>
+        <Nav.Link style={{'color':'black', "fontFamily": "sans-serif"}} href="/1"><strong>World</strong></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link style={{'color':'black'}} href="/2"><strong>Business</strong></Nav.Link>
+        <Nav.Link style={{'color':'black', "fontFamily": "sans-serif"}} href="/2"><strong>Business</strong></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-      <Nav.Link style={{'color':'black'}} href="/3"><strong>Technology</strong></Nav.Link>
+      <Nav.Link style={{'color':'black', "fontFamily": "sans-serif"}} href="/3"><strong>Technology</strong></Nav.Link>
         
       </Nav.Item>
       <Nav.Item>
-      <Nav.Link style={{'color':'black'}} href="/4"><strong>Entertainment</strong></Nav.Link>
+      <Nav.Link style={{'color':'black', "fontFamily": "sans-serif"}} href="/4"><strong>Entertainment</strong></Nav.Link>
       </Nav.Item>
     </Nav>
   );
